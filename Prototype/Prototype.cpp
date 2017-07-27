c0d4116d195243a915444a54e4edb58a94dc9f02
vicktcfj <315516440@qq.com>
2017-07-27 16:51:36 +0800

    提交Prototype Pattern

---
+++ Prototype/Prototype.cpp
@@ -0,0 +1,44 @@
+﻿/*
+ * 原型模式：
+ * 用原型实例指定创建对象的种类，并且通过拷贝这些原型创建新的对象
+ */
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Prototype
+{
+public:
+    virtual ~Prototype()
+    {
+    }
+    virtual unique_ptr<Prototype> clone() = 0;
+};
+
+class ConcretePrototype : public Prototype
+{
+public:
+    ConcretePrototype()
+    {
+    }
+
+    ConcretePrototype(const ConcretePrototype &obj)
+    {
+        cout << "ConcretePrototype copy" << endl;
+    }
+
+    virtual unique_ptr<Prototype> clone() override
+    {
+        return make_unique<ConcretePrototype>(*this);
+    }
+};
+
+int main()
+{
+    unique_ptr<Prototype> p = make_unique<ConcretePrototype>();
+    unique_ptr<Prototype> clone1 = p->clone();
+    unique_ptr<Prototype> clone2 = p->clone();
+
+    return 0;
+}
