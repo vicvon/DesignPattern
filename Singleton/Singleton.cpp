b2cd69575ffd2e3bf1b85c886fc6f4f72431872e
vicktcfj <315516440@qq.com>
2017-08-04 14:10:17 +0800

    commit Singleton Pattern

---
+++ Singleton/Singleton.cpp
@@ -0,0 +1,27 @@
+﻿/*
+ * 单例模式：
+ * 保证一个类仅有一个实例，并提供一个访问它的全局访问点。
+ */
+#include <iostream>
+
+using namespace std;
+
+class Singleton
+{
+public:
+    static Singleton & getInstance()
+    {
+        static Singleton singleton;
+        return singleton;
+    }
+private:
+    Singleton() = default;
+    Singleton(const Singleton &) = default;
+    Singleton & operator = (const Singleton &) = default;
+};
+
+int main()
+{
+    Singleton& s = Singleton::getInstance();
+    return 0;
+}
