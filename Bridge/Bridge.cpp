5aab0bd4b5450227abc6b2fe45730a6021292dc1
vicktcfj <315516440@qq.com>
2017-08-04 16:25:47 +0800

    commit Bridge Pattern and Command Pattern

---
+++ Bridge/Bridge.cpp
@@ -0,0 +1,68 @@
+﻿/*
+ * 桥接模式：
+ * 将抽象部分与它的实现部分分离，使它们都可以独立的变化。
+ */
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Implementor
+{
+public:
+    virtual void Operation() = 0;
+};
+
+class ConcreteImplementorA : public Implementor
+{
+public:
+    virtual void Operation() override
+    {
+        cout << "ConcreteImplementorA Operation" << endl;
+    }
+};
+
+class ConcreteImplementorB : public Implementor
+{
+public:
+    virtual void Operation() override
+    {
+        cout << "ConcreteImplementorB Operation" << endl;
+    }
+};
+
+class Abstraction
+{
+public:
+    void setImplementor(unique_ptr<Implementor>&& implementor)
+    {
+        implementor_ = move(implementor);
+    }
+
+    virtual void Operation() = 0;
+protected:
+    unique_ptr<Implementor> implementor_;
+};
+
+class RefinedAbstraction : public Abstraction
+{
+public:
+    virtual void Operation() override
+    {
+        cout << "RefinedAbstraction Operation" << endl;
+        implementor_->Operation();
+    }
+};
+
+int main()
+{
+    unique_ptr<Abstraction> refined = make_unique<RefinedAbstraction>();
+
+    refined->setImplementor(make_unique<ConcreteImplementorA>());
+    refined->Operation();
+
+    refined->setImplementor(make_unique<ConcreteImplementorB>());
+    refined->Operation();
+
+    return 0;
+}
---
+++ Command/Command.cpp
@@ -0,0 +1,68 @@
+﻿/*
+ * 命令模式：
+ * 将一个请求封装为一个对象，从而使你可用不同的请求对客户进行参数化；
+ * 对请求排队或记录请求日志，以及支持可撤销的操作。
+ */
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Receiver
+{
+public:
+    void Action()
+    {
+        cout << "Receiver Action" << endl;
+    }
+};
+
+class Command
+{
+public:
+    Command(unique_ptr<Receiver>&& receiver) : receiver_(move(receiver))
+    {
+    }
+
+    virtual void Execute() = 0;
+protected:
+    unique_ptr<Receiver> receiver_;
+};
+
+class ConcreteCommand : public Command
+{
+public:
+    ConcreteCommand(unique_ptr<Receiver>&& receiver) : Command(move(receiver))
+    {
+    }
+
+    virtual void Execute() override
+    {
+        cout << "ConcreteCommand Execute Command" << endl;
+        receiver_->Action();
+    }
+};
+
+class Invoker
+{
+public:
+    Invoker(unique_ptr<Command>&& command) : command_(move(command))
+    {
+    }
+    void Call()
+    {
+        cout << "Invoker Command" << endl;
+        command_->Execute();
+    }
+private:
+    unique_ptr<Command> command_;
+};
+
+int main()
+{
+    unique_ptr<Invoker> invoker = make_unique<Invoker>(make_unique<ConcreteCommand>(make_unique<Receiver>()));
+
+    invoker->Call();
+
+    return 0;
+}
